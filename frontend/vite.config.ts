00d2baf5b59262aca168e9260242d253cf44c342
mooenz <joss92821@hotmail.com>
2026-03-27 18:44:44 -0500

    chore: actualizar configuración de TypeScript y Vite para mejorar la estructura del proyecto

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,24 @@
+// @ts-check
 import { defineConfig } from 'vite'
+import { fileURLToPath, URL } from 'node:url'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
+  base: '/',
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
 })
